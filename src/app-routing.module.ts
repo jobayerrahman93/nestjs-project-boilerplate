a09b29cff9f266fc8fdc531a3f322b313b529f6e
jobayercse71 <rahmanjobayer71@gmail.com>
2023-07-15 15:57:38 +0600

    send email otp & match email otp & forget password done

--- src/app-routing.module.ts
+++ src/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { RouterModule } from '@nestjs/core';
+import { COMMON_ROUTES } from './common/common.router';
 import { USER_ROUTES } from './user/user.router';
 
-const Routes = [...USER_ROUTES];
+const Routes = [...USER_ROUTES, ...COMMON_ROUTES];
 @Module({
   imports: [RouterModule.register(Routes)],
   exports: [RouterModule],
---
+++ src/common/common.router.ts
@@ -0,0 +1,8 @@
+import { CommonModule } from './common.module';
+
+export const COMMON_ROUTES = [
+  {
+    path: 'api/v1/',
+    module: CommonModule,
+  },
+];
